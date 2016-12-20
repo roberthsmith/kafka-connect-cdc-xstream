0f0e04f529744698adef4c58345de6962c1224bf
Jeremy Custenborder <jcustenborder@gmail.com>
2016-12-20 02:04:36 -0600

    More wip. Consolidation around Json testing classes and jdbc.

--- src/test/java/io/confluent/kafka/connect/cdc/xstream/Oracle11gKeyMetadataProviderTests.java
+++ src/test/java/io/confluent/kafka/connect/cdc/xstream/Oracle11gKeyMetadataProviderTests.java
@@ -2,7 +2,7 @@ package io.confluent.kafka.connect.cdc.xstream;
 
 import com.google.common.collect.ImmutableSet;
 import org.hamcrest.core.IsEqual;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -12,7 +12,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Set;
 
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Disabled
 public class Oracle11gKeyMetadataProviderTests extends Oracle11gTest {
@@ -21,7 +21,7 @@ public class Oracle11gKeyMetadataProviderTests extends Oracle11gTest {
   Connection connection;
   Oracle11gKeyMetadataProvider keyMetadataProvider;
 
-  @Before
+  @BeforeAll
   public void before() throws SQLException {
     this.connection = Utils.openConnection(jdbcUrl, DockerUtils.USERNAME, DockerUtils.PASSWORD);
     this.keyMetadataProvider = new Oracle11gKeyMetadataProvider(this.connection);
@@ -31,28 +31,28 @@ public class Oracle11gKeyMetadataProviderTests extends Oracle11gTest {
   public void findPrimaryKey() throws SQLException {
     Set<String> expectedKeys = ImmutableSet.of("USER_ID");
     Set<String> actualKeys = this.keyMetadataProvider.findPrimaryKey("CDC_TESTING", "PRIMARY_KEY_TABLE");
-    assertThat("actualKeys did not match.", actualKeys, IsEqual.equalTo(expectedKeys));
+    assertEquals(actualKeys, expectedKeys, "actualKeys did not match.");
 
     expectedKeys = ImmutableSet.of();
     actualKeys = this.keyMetadataProvider.findPrimaryKey("CDC_TESTING", "UNIQUE_INDEX_TABLE");
-    assertThat("actualKeys did not match.", actualKeys, IsEqual.equalTo(expectedKeys));
+    assertEquals(actualKeys, expectedKeys, "actualKeys did not match.");
   }
 
   @Test
   public void findUniqueKey() throws SQLException {
     Set<String> expectedKeys = ImmutableSet.of("FIRST_COLUMN", "SECOND_COLUMN");
     Set<String> actualKeys = this.keyMetadataProvider.findUniqueKey("cdc_testing", "UNIQUE_INDEX_TABLE");
-    assertThat("actualKeys did not match.", actualKeys, IsEqual.equalTo(expectedKeys));
+    assertEquals(actualKeys, IsEqual.equalTo(expectedKeys), "actualKeys did not match.");
 
     expectedKeys = ImmutableSet.of();
     actualKeys = this.keyMetadataProvider.findUniqueKey("CDC_TESTING", "NO_INDEXES");
-    assertThat("actualKeys did not match.", actualKeys, IsEqual.equalTo(expectedKeys));
+    assertEquals(actualKeys, expectedKeys, "actualKeys did not match.");
   }
 
   @Test
   public void findKeys() throws SQLException {
     Set<String> expectedKeys = ImmutableSet.of("FIRST_COLUMN", "SECOND_COLUMN");
     Set<String> actualKeys = this.keyMetadataProvider.findKeys("cdc_testing", "UNIQUE_INDEX_TABLE");
-    assertThat("actualKeys did not match.", actualKeys, IsEqual.equalTo(expectedKeys));
+    assertEquals(actualKeys, expectedKeys, "actualKeys did not match.");
   }
 }
